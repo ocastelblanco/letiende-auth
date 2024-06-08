9fbc3a53b109a3a202642e8fe85116ad01e9e7ed
Oliver Castelblanco <ocastelblanco@gmail.com>
2024-06-07 19:20:44 -0500

    feat: pedidos app

---
+++ src/app/apps/pedidos/pedidos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lta-pedidos',
+  standalone: true,
+  imports: [],
+  templateUrl: './pedidos.component.html',
+  styleUrl: './pedidos.component.scss'
+})
+export class PedidosComponent {
+
+}
